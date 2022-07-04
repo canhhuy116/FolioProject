92b28ca02a6535d3c86f1698010254e7e1367294
canhhuy116 <canhhuy1162002@gmail.com>
2022-07-04 22:02:46 +0700

    update image header

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,7 +1,7 @@
 const images = {
     logo: require('./logo.svg').default,
     HeaderBG: require('./HeaderBg.svg').default,
-    HeroImg: require('./hero img.svg').default,
+    HeroImg: require('./hero img.png'),
     HeroBG: require('./heroBg.svg').default,
     CircleArrow: require('./CircleArrow.svg').default,
 };
