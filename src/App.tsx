8358d6aff85f88685b880c692e2ab63cc93bcb85
canhhuy116 <canhhuy1162002@gmail.com>
2022-07-04 21:33:59 +0700

    Add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/styles/App.scss'
+import images from './assets/images';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Header">
+        <div className="Navigation">
+          <div className="Nav__logo">
+            <img src={images.logo} alt="Logo" />
+          </div>
+          <ul className="Nav__menu">
+            <li className="Nav__menu-item">About</li>
+            <li className="Nav__menu-item">Work</li>
+            <li className="Nav__menu-item">Services</li>
+            <li className="Nav__menu-item">Jobs</li>
+          </ul>
+          <span className="Nav__notify">2</span>
+          <button className="button-primary">
+            <span className="Recent-Works">Contact Us</span>
+          </button>
+        </div>
+        <div className="Header__Container">
+          <div className="Header__HeroContent">
+            <div className='Header__HeroContent__main'>
+              <h2 className="Header__HeroContent__title">
+                Stand Out from The Crowd.
+              </h2>
+              <p className="Header__HeroContent__content">
+                Agency is a full-service agency, busy designing and building beautiful digital products, brands, and experiences.
+              </p>
+            </div>
+            <button className="button-primary">
+              <span className="Recent-Works">
+                Recent Work
+              </span>
+            </button>
+          </div>
+          <img src={images.HeroBG} alt="HeroBG" className='Header__HeroBG' />
+          <img src={images.HeroImg} alt="HeroImg" className='Header__HeroImg' />
+          <div className="Header__ScrollDownIcon">
+            <img src={images.CircleArrow} alt=""/>
+            <span>Scroll down</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/assets/images/index.ts
@@ -0,0 +1,9 @@
+const images = {
+    logo: require('./logo.svg').default,
+    HeaderBG: require('./HeaderBg.svg').default,
+    HeroImg: require('./hero img.svg').default,
+    HeroBG: require('./heroBg.svg').default,
+    CircleArrow: require('./CircleArrow.svg').default,
+};
+
+export default images;
