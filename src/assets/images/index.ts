23172d75bec60f6c16a77a9848ae3dbeb7fdc5eb
canhhuy116 <canhhuy1162002@gmail.com>
2022-07-05 11:08:27 +0700

    new branch Folio/About

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -4,6 +4,8 @@ const images = {
     HeroImg: require('./hero img.png'),
     HeroBG: require('./heroBg.svg').default,
     CircleArrow: require('./CircleArrow.svg').default,
+    shape1: require('./shape 1.svg').default,
+    logoSymbol: require('./logoSymbol.svg').default,
 };
 
 export default images;
