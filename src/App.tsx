9fd32439d0cff0aa38cfb8325972fcdef7f0db1a
canhhuy116 <canhhuy1162002@gmail.com>
2022-07-05 21:37:26 +0700

    finish Folio/About

--- src/App.tsx
+++ src/App.tsx
@@ -21,27 +21,118 @@ function App() {
             <span className="Recent-Works">Contact Us</span>
           </button>
         </div>
-        <div className="Header__Container">
-          <div className="Header__HeroContent">
-            <div className='Header__HeroContent__main'>
-              <h2 className="Header__HeroContent__title">
-                Stand Out from The Crowd.
-              </h2>
-              <p className="Header__HeroContent__content">
-                Agency is a full-service agency, busy designing and building beautiful digital products, brands, and experiences.
-              </p>
+        <div className='grid'>
+          <div className="Header__Container">
+            <div className="Header__HeroContent">
+              <div className='Header__HeroContent__main'>
+                <h2 className="Header__HeroContent__title">
+                  Stand Out from The Crowd.
+                </h2>
+                <p className="Header__HeroContent__content">
+                  Agency is a full-service agency, busy designing and building beautiful digital products, brands, and experiences.
+                </p>
+              </div>
+              <button className="button-primary">
+                <span className="Recent-Works">
+                  Recent Work
+                </span>
+              </button>
             </div>
-            <button className="button-primary">
-              <span className="Recent-Works">
-                Recent Work
-              </span>
-            </button>
+            <img src={images.HeroBG} alt="HeroBG" className='Header__HeroBG' />
+            <img src={images.HeroImg} alt="HeroImg" className='Header__HeroImg' />
+            <div className="Header__ScrollDownIcon">
+              <img src={images.CircleArrow} alt=""/>
+              <span>Scroll down</span>
+            </div>
           </div>
-          <img src={images.HeroBG} alt="HeroBG" className='Header__HeroBG' />
-          <img src={images.HeroImg} alt="HeroImg" className='Header__HeroImg' />
-          <div className="Header__ScrollDownIcon">
-            <img src={images.CircleArrow} alt=""/>
-            <span>Scroll down</span>
+        </div>
+      </div>
+      <div className="WhoWeAre">
+          {/* <div className="WhoWeAre__line">
+          </div> */}
+        <div className="WhoWeAre__shape3d">
+          <img src={images.shape1} alt="shape 3d" className='shape3dIMG' />
+          <div className='grid'>
+            <div className="Headline">
+              <img src={images.logoSymbol} alt="" className="Headline__logo" />
+              <div className="Headline__content">
+                <div className="Headline__content__tagline">
+                  <span className="taglineNumber">
+                    01
+                  </span> 
+                  <span className="taglineContent">
+                    Who We Are
+                  </span>
+                </div>
+                <h2 className="Headline__content__para">
+                  We Make Designs that Lead and Inpire.
+                </h2>
+              </div>
+              <div className="Headline__Arrow">
+                <img src={images.leftNormal} alt="Left Arrow" />
+                <img src={images.rightHover} alt="Right Arrow" />
+              </div>
+            </div>
+            <div className="WhoWeAre__body">
+              <ul className="WhoWeAre__body__timeLine">
+                <li className="TimeLine-item">2017</li>
+                <li className="TimeLine-item">2018</li>
+                <li className="TimeLine-item">2019</li>
+                <li className="TimeLine-item TimeLineCurrentYear">2020</li>
+                <li className="TimeLine-item">2021</li>
+              </ul>
+              <div className="WhoWeAre__body__img">
+                <img src={images.whoweareIMG} alt="Body img" />
+              </div>
+            </div>
+            <div className="WhoWeAre__footer">
+              <div className="WhoWeAre__footer__content">
+                <div className="WhoWeAre__footer__content__main">
+                  <h2 className="WhoWeAre__content__title">
+                    A design team building a curated marketplace for UI designers.
+                  </h2>
+                  <p className="WhoWeAre__content__body">
+                    4,404 curated design resources to energize your creative workflow. We're a growing family of 334,531 designers and makers from around the world
+                  </p>
+                </div>
+                <button className="button-primary">
+                  <span className="Recent-Works">Contact Us</span>
+                </button>
+              </div>
+              <div className="WhoWeAre__footer__number">
+                <h1 className="WhoWeAre__number">
+                  28
+                </h1>
+                <h6 className="WhoWeAre__number__content">
+                  Bigest Branding
+                </h6>
+              </div>
+            <div/>
+          </div>
+          </div>
+        </div>
+      </div>
+      <div className="Services">
+        <div className="grid">
+          <div className="Headline">
+            <img src={images.logoSymbol} alt="" className="Headline__logo" />
+            <div className="Headline__content">
+              <div className="Headline__content__tagline">
+                <span className="taglineNumber">
+                02.
+                </span> 
+                <span className="taglineContent">
+                  What We Do
+                </span>
+              </div>
+              <h2 className="Headline__content__para">
+                We Make Designs that Lead and Inpire.
+              </h2>
+            </div>
+            <div className="Headline__Arrow">
+              <img src={images.leftNormal1} alt="Left Arrow" />
+              <img src={images.rightHover} alt="Right Arrow" />
+            </div>
           </div>
         </div>
       </div>
--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -4,8 +4,12 @@ const images = {
     HeroImg: require('./hero img.png'),
     HeroBG: require('./heroBg.svg').default,
     CircleArrow: require('./CircleArrow.svg').default,
-    shape1: require('./shape 1.svg').default,
+    shape1: require('./shape1.svg').default,
     logoSymbol: require('./logoSymbol.svg').default,
+    rightHover: require('./right-hover.svg').default,
+    leftNormal: require('./left-normal.svg').default,
+    whoweareIMG: require('./whoweareIMG.png'),
+    leftNormal1: require('./left-normal(1).svg').default,
 };
 
 export default images;
